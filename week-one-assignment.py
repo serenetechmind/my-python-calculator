2a28306414fe09e7016856a92e781e789721b21c
serenetechmind <serenetechmind@gmail.com>
2026-05-16 15:04:26 +0100

    My week 1 assignment

---
+++ week-one-assignment.py
@@ -0,0 +1,34 @@
+#variable
+first_name="David"
+last_name="Will"
+age=21
+greeting="Good evening!"
+greeting_first="Hello!"
+sentence_a="My name is"
+sentence_b="and i am"
+person="Biola"
+intro="I am"
+end_point="years old"
+
+print(greeting, person, intro, age, end_point)
+print(type(first_name))
+print(type(last_name))
+print(type(age))
+
+#fix the errors
+#print("Hello Bola)
+#Name=Bola, print(name)
+#age="15", print(type(age))
+
+print("Hello Bola")
+#variable
+name="Bola"
+print(name)
+
+#variable
+age=15
+print(type(age))
+
+print(greeting_first, sentence_a, first_name, sentence_b, age, end_point)
+
+print(greeting_first, last_name, intro, name)
